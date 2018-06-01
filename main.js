6dcb16e282464023ce00b5977e4b94296c63e769
Kewde <kewde@particl.io>
2018-06-01 17:12:49 +0000

    add preload & contestIsolation to browserwindow

--- main.js
+++ main.js
@@ -7,7 +7,14 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true,
+      preload: 'preload.js'
+    }
+  });
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
